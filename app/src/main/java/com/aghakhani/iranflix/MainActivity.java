fc6e924a56a3edc1ebcceb0bc6c08723b74aefde
Kiarash Aghakhani <kiarash88ca@gmail.com>
2025-01-13 21:12:09 -0800

    List View Implementation

---
+++ app/src/main/java/com/aghakhani/iranflix/MainActivity.java
@@ -0,0 +1,60 @@
+package com.aghakhani.iranflix;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity {
+
+    String fruitNames[] = {"Banana", "Grape", "Guava", "Mango", "Orange", "Watermelon"};
+    String fruitMessage[] = {"Banana is very Good", "Grape is very Good", "Guava is very Good", "Mango is very Good", "Orange is very Good", "Watermelon is very Good"};
+
+    int fruitImageIds[] = {R.drawable.banana,
+            R.drawable.grape,
+            R.drawable.guava,
+            R.drawable.mango,
+            R.drawable.orange,
+            R.drawable.watermelon};
+    ListView listView ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        listView = findViewById(R.id.listView);
+        ArrayList list = new ArrayList();
+        for ( int i = 0; i < fruitImageIds.length; i++) {
+
+            HashMap<String, Object> map = new HashMap<>();
+            map.put("fruitName", fruitNames[i]);
+            map.put("fruitImage", fruitImageIds[i]);
+            map.put("fruitMessage", fruitMessage[i]);
+            list.add(map);
+        }
+        String[] from = {"fruitName","fruitMessage", "fruitImage"};
+        int to[] = {R.id.textViewTitle,R.id.textViewMessage, R.id.imageView};
+
+        SimpleAdapter simpleAdapter = new SimpleAdapter(this, list, R.layout.listview_items,from,to);
+        listView.setAdapter(simpleAdapter);
+
+
+
+
+    }
+}
